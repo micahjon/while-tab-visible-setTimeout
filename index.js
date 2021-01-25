6a759ca838063eb35f91067efbb12efa53439c49
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2021-01-24 20:07:20 -0800

    First commit. Needs tested

---
+++ index.js
@@ -0,0 +1,70 @@
+/**
+ * Create a timer (similar to setTimeout) that only runs when tab is visible
+ * @param {function} callback
+ * @param {number} timeoutMs
+ * @return {function} clearTimeout
+ */
+export default function whileTabVisibleTimeout(callback, timeoutMs) {
+    let timeoutId;
+    let cleanupListener;
+
+    if (isTabVisible()) {
+        // Tab started off visible
+
+        // Keep track of start time in case we need to pause timeout
+        const startTime = Date.now();
+
+        cleanupListener = whenNotVisible(() => {
+            // Tab was hidden before timeout completed
+            clearTimeout(timeoutId);
+            // Wait until tab is visible again to restart timeout
+            whileTabVisibleTimeout(callback, timeoutMs - (Date.now() - startTime));
+        });
+
+        // Start timer
+        timeoutId = setTimeout(() => {
+            cleanupListener();
+            callback();
+        }, timeoutMs);
+    } else {
+        // Tab started off hidden
+        cleanupListener = whenVisible(() => {
+            // Tab became visible. Start timer now.
+            whileTabVisibleTimeout(callback, timeoutMs);
+        });
+    }
+
+    return clearTimeout;
+
+    function clearTimeout() {
+        cleanup();
+        clearTimeout(timeoutId);
+    }
+}
+
+function whenVisible(callback) {
+    return onVisibilityChange(true, callback);
+}
+
+function whenNotVisible(callback) {
+    return onVisibilityChange(false, callback);
+}
+
+function onVisibilityChange(shouldBeVisible, callback) {
+    document.addEventListener('visibilitychange', function onVisibilityChange() {
+        if (shouldBeVisible === isTabVisible()) {
+            cleanup();
+            callback();
+        }
+    });
+
+    return cleanup;
+
+    function cleanup() {
+        document.removeEventListener('visibilitychange', onVisibilityChange);
+    }
+}
+
+function isTabVisible() {
+    return document.visibilityState === 'visible';
+}
